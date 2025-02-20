279de661260e96afa5891177ff780cefb23cee50
Dmitry3Lyalyuk <165943906+Dmitry3Lyalyuk@users.noreply.github.com>
2025-02-20 23:09:02 +0300

    add DI Validation

--- BookManagement.Application/Books/Validators/CreateBookCommandValidator.cs
+++ BookManagement.Application/Books/Validators/CreateBookCommandValidator.cs
@@ -1,16 +1,22 @@
 ﻿using BookManagement.Application.Books.Commands.Create;
+using BookManagement.Application.Common;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookManagement.Application.Books.Validators
 {
     public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
     {
-        public CreateBookCommandValidator()
+        private readonly IAppicationDbContext _context;
+        public CreateBookCommandValidator(IAppicationDbContext context)
         {
+            _context = context;
+
             RuleFor(t => t.Title)
                 .MaximumLength(50)
                 .NotEmpty()
-                .WithMessage("Title is required");
+                .WithMessage("Title is required")
+                .MustAsync(BeUniqueTitle).WithMessage("Title shuld be unique.");
 
             RuleFor(p => p.PublicationYear)
                 .LessThan(4)
@@ -23,6 +29,11 @@ namespace BookManagement.Application.Books.Validators
                 .MaximumLength(100)
                 .NotEmpty()
                 .WithMessage("AuthorName is required");
+        }
+
+        private async Task<bool> BeUniqueTitle(string name, CancellationToken cancellationToken)
+        {
+            return !await _context.Books.AnyAsync(t => t.Title == name, cancellationToken);
         }
     }
 }
--- BookManagement.Application/Books/Validators/UpdateBookCommandValidator.cs
+++ BookManagement.Application/Books/Validators/UpdateBookCommandValidator.cs
@@ -11,22 +11,22 @@ namespace BookManagement.Application.Books.Validators
         {
             _context = context;
 
-            RuleFor(t => t.Title)
-                 .MaximumLength(50)
-                 .NotEmpty()
-                 .WithMessage("Title is required");
+            //RuleFor(t => t.Title)
+            //     .MaximumLength(50)
+            //     .NotEmpty()
+            //     .WithMessage("Title is required");
 
-            RuleFor(p => p.PublicationYear)
-                .LessThan(4)
-                .WithMessage("Title is required")
-                .NotEmpty()
-                .WithMessage("PublicationYear is required");
+            //RuleFor(p => p.PublicationYear)
+            //    .LessThan(4)
+            //    .WithMessage("Title is required")
+            //    .NotEmpty()
+            //    .WithMessage("PublicationYear is required");
 
-            RuleFor(a => a.AuthorName)
-                .MinimumLength(1)
-                .MaximumLength(100)
-                .NotEmpty()
-                .WithMessage("AuthorName is required");
+            //RuleFor(a => a.AuthorName)
+            //    .MinimumLength(1)
+            //    .MaximumLength(100)
+            //    .NotEmpty()
+            //    .WithMessage("AuthorName is required");
         }
     }
 }
